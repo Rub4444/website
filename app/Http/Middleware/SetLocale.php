f47c59dfc5b28f15c75a71876fef52290c50921d
Ruben <isahakyan06@gmail.com>
2025-06-08 14:52:47 +0400

    locale hy in middlweare

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -16,7 +16,7 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next): Response
     {
-        App::setLocale(session('locale'));
+        App::setLocale(session('locale', config('app.locale')));
         return $next($request);
     }
 }
