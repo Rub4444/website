391ad457f79e864e5404769c44099cc85395adad
Ruben <isahakyan06@gmail.com>
2025-06-14 17:56:36 +0400

    headers 2

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -9,6 +9,7 @@ class SecureHeaders
 {
     public function handle(Request $request, Closure $next)
     {
+        \Log::info('SecureHeaders middleware triggered');
         $response = $next($request);
 
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
