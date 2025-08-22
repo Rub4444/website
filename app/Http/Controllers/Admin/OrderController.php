c3f3d34007a39f2367d76eb29c3ba03cbab34511
Ruben <isahakyan06@gmail.com>
2025-08-22 14:37:22 +0400

    mail to confirmed order40022

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Mail\OrderConfirmed;
 use Illuminate\Support\Facades\Mail;
 
 class OrderController extends Controller
