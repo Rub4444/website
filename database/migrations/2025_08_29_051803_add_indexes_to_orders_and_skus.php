aea50157269f1887a7547e865bbff1de093928f6
Ruben <isahakyan06@gmail.com>
2025-08-29 05:34:23 +0400

    worker

--- database/migrations/2025_08_29_051803_add_indexes_to_orders_and_skus.php
+++ database/migrations/2025_08_29_051803_add_indexes_to_orders_and_skus.php
@@ -6,26 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void {
-        Schema::table('orders', function (Blueprint $table) {
-            $table->index('user_id');
-            $table->index('created_at');
+
+
+        Schema::table('products', function (Blueprint $table) {
+            $table->index('category_id');
+            $table->index('code');
         });
 
-        Schema::table('skus', function (Blueprint $table) {
-            $table->index('category_id');
-            $table->index('sku_id');
-        });
     }
 
     public function down(): void {
-        Schema::table('orders', function (Blueprint $table) {
-            $table->dropIndex(['user_id']);
-            $table->dropIndex(['created_at']);
-        });
 
-        Schema::table('skus', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->dropIndex(['category_id']);
-            $table->dropIndex(['sku_id']);
+            $table->dropIndex(['code']);
+
         });
     }
 };
